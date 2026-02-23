b4af2286f6b48a1d99808dd0a33c51fa93c6a8c8
Econ Account <ouecon008@schooner1.oscer.ou.edu>
2026-02-22 21:03:57 -0600

    Completed PS4

---
+++ ProblemSets/PS4/PS4a_Nguyen.R
@@ -0,0 +1,32 @@
+# Practice with JSON files
+
+# Load/Install Packages
+# install.packages("jsonlite")
+# install.packages("tidyverse")
+
+library(jsonlite)
+library(tidyverse)
+
+#(a) Download the JSON file
+system('wget -O dates.json "https://www.vizgr.org/historical-events/search.php?format=json&begin_date=00000101&end_date=20240209&lang=en"')
+
+#(b) Print the file to the console
+
+# system("cat dates.json")
+
+#(c) Clean the data
+mylist <- fromJSON('dates.json')
+mydf <- bind_rows(mylist$result[-1])
+
+#(d) What type of object is mydf
+class(mydf)
+
+#(e) First nth rows
+
+# First 10 rows
+head(mydf, 10)
+
+# First 20 rows
+head(mydf, 20)
+
+
